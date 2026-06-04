c9f8b8188b8c0c25516cb250f936a7c7fa2b1724
Shivam Kumar Chaubey <shivamc36@gmail.com>
2026-06-04 17:10:53 +0530

    security: register 429 handler

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+handler429 = 'contracts.views.handler429'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contracts.urls')),
